12ff32f16c10980308de08d436eb92450a842dd5
Flemming K. Jensen <flemming.k.jensen@gmail.com>
2020-04-20 11:27:38 +0200

    indført spændende for-løkke

--- Main.java
+++ Main.java
@@ -3,5 +3,12 @@ public class Main {
 
   public static void Main(String[] args) {
     System.out.println("Hello World of Git!");
+    System.out.println("endnu en linie");
+
+    int x=5;
+    for (int i=0; i<3; i++)
+      x += 8;
+
+    System.out.println(x);
   }
 }
