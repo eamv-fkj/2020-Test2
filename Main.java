a8615fbc274e3ec50409418e4ee8cf5861b874ee
Flemming K. Jensen <flemming.k.jensen@gmail.com>
2020-04-20 13:10:26 +0200

    indlæsning af linie tekst

--- Main.java
+++ Main.java
@@ -2,13 +2,8 @@
 public class Main {
 
   public static void Main(String[] args) {
-    System.out.println("Hello World of Git!");
-    System.out.println("endnu en linie");
+    String input = System.in.readLine();
 
-    int x=5;
-    for (int i=0; i<3; i++)
-      x += 8;
-
-    System.out.println(x);
+    System.out.println(input);
   }
 }
